b25a6fa687cd2d740b24cd09d28a8cdfa2a9c705
Joao Bioco <hbioco@hotmail.com>
2019-02-09 21:46:39 +0000

    virtual

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/joaobioco/Documents/myrealprojects2019/hrDjangoProject/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
